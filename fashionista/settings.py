f5ec7452d33c859bd25c0de8b8e64e79bc420509
Atinos31 <atinosandra2014@gmail.com>
2021-11-16 18:51:43 +0000

    completed homepage header and css

--- fashionista/settings.py
+++ fashionista/settings.py
@@ -148,8 +148,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-# Default primary key field type
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
