6341578b5753a806041c8191289b00a4f7bb81d6
Atinos31 <atinosandra2014@gmail.com>
2021-12-20 20:52:23 +0000

    added order history and checkout views

--- cart/contexts.py
+++ cart/contexts.py
@@ -33,7 +33,7 @@ def cart_contents(request):
                 })
         
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
