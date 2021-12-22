aa87bfd1f5180f8d29fb5726f59e36371b1f52c8
Atinos31 <atinosandra2014@gmail.com>
2021-12-22 22:21:04 +0000

    favourite app - add_to_favourite,views,models,template added

--- favourite/views.py
+++ favourite/views.py
@@ -2,23 +2,38 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from profiles.models import UserProfile
 from products.models import Product
-from .models import WishList
+from .models import Favourite
 
 
 @login_required
 def favourite(request):
     """
-    A view to render the users favourites
+    A view to render the user's wishlist
     """
-    profile = get_object_or_404(UserProfile, user=request.user)
-    products = profile.favourite.all()
-    template = 'favourite/favourite.html'
+    favourite = None
+    try:
+        favourite = Favourite.objects.get(user=request.user)
+    except Favourite.DoesNotExist:
+        pass
+
     context = {
-        'products': products,
-        'profile': profile,
+        'favourite': favourite,
     }
 
-    return render(request, template, context)
+    return render(request, 'favourite/favourite.html', context)
+
+
+@login_required
+def add_to_favourite(request, product_id):
+    """
+    Add a product from the store to 
+    favourites for the logged in user
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    favourite = Favourite.objects.get_or_create(user=request.user)
+    favourite.product.add(product)
+
 
 
 @login_required
@@ -29,6 +44,10 @@ def add_to_favourite(request, product_id):
     """
     product = get_object_or_404(Product, pk=product_id)
 
-    favourite = Favourite.objects.get_or_create(user=request.user)
-    favourite.product.add(product)
+    # Create a wishlist for the user if they don't have one
+    favourite, _ = Favourite.objects.get_or_create(user=request.user)
+    # Add product to the wishlist
+    favourite.products.add(product)
+    messages.info(request, "A new product was added to your wishlist")
 
+    return redirect(request.META.get('HTTP_REFERER'))
